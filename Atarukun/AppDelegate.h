f846f12151dd0ecd0f3c491b7635eeaf39514fd2
大山 <oyama@mgt139-no-Mac-mini.local>
2014-06-10 13:22:47 +0900

    Initial Commit

---
+++ Atarukun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Atarukun
+//
+//  Created by 大山 on 2014/06/10.
+//  Copyright (c) 2014年 oyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Atarukun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Atarukun
+//
+//  Created by 大山 on 2014/06/10.
+//  Copyright (c) 2014年 oyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
